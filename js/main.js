42f6848251e791b8a792df82de3e267c9150ad7c
VioletM <violetmedina@gmail.com>
2022-03-16 14:50:11 -0500

    multi cards with links

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
+let body = document.querySelector('body')
+let container = document.querySelector('div')
+let recTitle = document.querySelector('div')
+
+body.appendChild(container)
+
 
 // fetch(`https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction`)
 
 // let movietvTitleFetch = title => {
-    fetch(`https://imdb-api.com/en/API/SearchTitle/${key}/the patriot`)
-    .then(result => result.json())
-    .then(data => {
-        console.log(data.results);
+//     fetch(`https://imdb-api.com/en/API/SearchTitle/${imdbKey}/the patriot`)
+//     .then(result => result.json())
+//     .then(data => {
+//         console.log(data.results);
 
-        data.results.forEach(element => {
-            console.log(element.title, element.description, element.id);
+//         data.results.forEach(element => {
+//             console.log(element.title, element.description, element.id);
 
         
-    });
+//     });
 
-})
+// })
 // }
 
 
 // fetch(`https://imdb-api.com/en/API/Title/${key}/tt1375666/`)
 
-
-let body = document.querySelector('body')
-let container = document.querySelector('div')
-
-body.appendChild(container)
 
 let tasteDiveFetch = () => {
     fetch("https://tastedive.com/api/similar?q=the+patriot%2C+the+patriot+act") //https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction
@@ -53,18 +54,38 @@ let titleFetch = ttCode => {
 
 
 
-
-const clearInput = () => {
+// search bar function to clear entered text
+const clearInput = () => {  
     const input = document.getElementsByTagName("input")[0];
     input.value = "";
 }
-
 const clearBtn = document.getElementById("clear-btn");
 clearBtn.addEventListener("click", clearInput);
+// end of search clear code
 
-const card = document.querySelector(".card__inner");
+// card flip code
+const card1 = document.querySelector(".cardJs1");
+const card2 = document.querySelector(".cardJs2");
+const card3 = document.querySelector(".cardJs3");
+const card4 = document.querySelector(".cardJs4");
 
-card.addEventListener("click", function (e) {
-    card.classList.toggle('is-flipped');
+card1.addEventListener("click", function (e) {
+    card1.classList.toggle('is-flipped');
+});
+card2.addEventListener("click", function (e) {
+    card2.classList.toggle('is-flipped');
+});
+card3.addEventListener("click", function (e) {
+    card3.classList.toggle('is-flipped');
+});
+card4.addEventListener("click", function (e) {
+    card4.classList.toggle('is-flipped');
 });
 
+// let card = document.getElementsByClassName('card')
+
+// card.addEventListener('click', event => {
+//     if(event.target.classList.contains('cardJs')) {
+//         event.target.classList.toggle('is-flipped')
+//     }
+// })
