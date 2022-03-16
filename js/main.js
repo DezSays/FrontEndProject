8bfa1e1f8f5a3fb9f5eff9247e09572c4ec9bcaf
Dez <dezareabryan@gmail.com>
2022-03-16 15:40:29 -0400

    worked on js file, but it needs to be reset so ignore these changes.

--- js/main.js
+++ js/main.js
@@ -2,20 +2,27 @@
 // fetch(`https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction`)
 
 // let movietvTitleFetch = title => {
-    fetch(`https://imdb-api.com/en/API/SearchTitle/${key}/the patriot`)
-    .then(result => result.json())
-    .then(data => {
-        console.log(data.results);
+//     fetch(`https://imdb-api.com/en/API/SearchTitle/${key}/the patriot`)
+//     .then(result => result.json())
+//     .then(data => {
+//         console.log(data.results);
 
-        data.results.forEach(element => {
-            console.log(element.title, element.description, element.id);
+//         let ul = document.querySelector("ul")
+
+//         data.results.forEach(element => {
+//             console.log(element.title, element.description, element.id);
+
+//             `<li><a href=#>${element.title} ${element.description}</li></a>`
+
+            
 
         
-    });
+//     });
 
-})
+    
+// })
 // }
-
+// movietvTitleFetch()
 
 // fetch(`https://imdb-api.com/en/API/Title/${key}/tt1375666/`)
 
@@ -67,4 +74,18 @@ const card = document.querySelector(".card__inner");
 card.addEventListener("click", function (e) {
     card.classList.toggle('is-flipped');
 });
+
+
+
+const searchMovieTitleTrial = async () => {
+
+    let movieArr = [];
+    let pageIndex = 1;
+    
+    let movieTitle = await fetch(`https://imdb-api.com/en/API/SearchTitle/${key}/the patriot`)
+
+    let resultsArr = await movieTitle.json();
+    console.log(resultsArr);
+}
+searchMovieTitleTrial()
 
